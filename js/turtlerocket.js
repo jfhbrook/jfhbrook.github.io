105ef6615dc6d434e9fcbf0c06b392e8c6f320b6
Joshua Holbrook <josh.holbrook@gmail.com>
2016-11-13 13:06:07 -0500

    Stuff that should with work, well, work

--- js/turtlerocket.js
+++ js/turtlerocket.js
@@ -1,3 +1,26 @@
 function turtlerocket(user, readSelect, writeSelect) {
-  writeSelect.append('<ul><li>Hi</li></ul>');
+  var text = '';
+
+  var fullText = readSelect.text().split('\n');
+
+  var i = 0;
+  while (!text && i < fullText.length) {
+    text = fullText[i];
+    i++;
+  }
+
+
+  var uri = 'https://turtlerocket-2016.nodeknockout.com/api/v1/search?user='+encodeURIComponent(user)+'&q='+encodeURIComponent(text);
+
+  $.get(uri, function(data) {
+    console.log(data);
+    if (data.hits) {
+      writeSelect.append('<ul>' + data.hits.reduce(function(txt, result) {
+        txt += '<li><a href=" + result.link + '">'+result.title+'</a></li>';
+      }, '') + '</ul>')
+    }
+    else {
+      writeSelect.append('<ul><li>hi</li></ul>')
+    }
+  });
 }
